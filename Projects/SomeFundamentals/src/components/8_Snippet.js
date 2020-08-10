7e989be13f03ec30022d7b7e9de38ec22c190f4c
pedro-pita <hpp.henriquepita@gmail.com>
2020-08-10 14:38:10 +0100

    Create Snippet VSCode

---
+++ Projects/SomeFundamentals/src/components/8_Snippet.js
@@ -0,0 +1,12 @@
+//CREATE SNIPPED 
+//  File -> Preferences -> User Snippets -> New Snippet ... 
+//RUN SNIPPED
+//  Write the snippet name and click enter
+import React from 'react'
+import { Text } from 'react-native'
+
+export default props => {
+    return (
+        <Text></Text>
+    )
+}
